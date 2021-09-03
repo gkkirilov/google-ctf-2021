591e7e9375aabe3fa86607f0f25c2a5ba1ed89af
Georgi Kirilov <gkkirilov@gmail.com>
2021-09-03 17:46:36 +0000

    Update repo

---
+++ 3.speedchase.js
@@ -0,0 +1,8 @@
+function speedChase(dataArray) {
+    console.log(dataArray)
+
+}
+
+
+var dataArray = [0, 0, 3.25, 3.25, 3.25, 3, 3, 3, 44.75, 44.75, 44.75, 0, 0, 0, 0, 0, 0]
+speedChase(dataArray)
